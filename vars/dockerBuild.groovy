a468673ce940bbacb0c1dfe4d2c116b562b8c3f9
Kailas <dawarekailas555@gmail.com>
2024-10-15 23:12:43 +0530

    code updated docker ecr create scan psh

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -10,7 +10,14 @@
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
-     docker build -t ${ecr_repoName} .
-     docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:${BUILD_NUMBER}
+     docker build -t ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:${BUILD_NUMBER} .
     """
 }
+
+// docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:${BUILD_NUMBER}
+
+
+    // sh """
+    //  docker build -t ${ecr_repoName} .
+    //  docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:${BUILD_NUMBER}
+    // """
