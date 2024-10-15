5e11923205af62ea2847c0dee09c185c4d16abfb
Kailas <dawarekailas555@gmail.com>
2024-10-15 23:38:24 +0530

    code updated docker ecr create scan psh

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -11,5 +11,6 @@ def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
      docker rmi ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:${BUILD_NUMBER}
+     aws ecr batch-delete-image --repository-name ${ecr_repoName} --image-ids imageTag=${BUILD_NUMBER} --region ${region}
     """
 }
